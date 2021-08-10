4365967c151a796f7dd5036e300225bf7e357479
Roberto <robertomoraes@lampp-it.com.br>
2021-08-10 17:24:11 -0500

    implementação do metho de busca por produtos e contatos

--- Contatos/src/main/java/com/contatos/Contatos/controller/ProdutoController.java
+++ Contatos/src/main/java/com/contatos/Contatos/controller/ProdutoController.java
@@ -3,8 +3,13 @@ package com.contatos.Contatos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,5 +31,13 @@ public class ProdutoController {
 	    modelAndView.addObject("produtos", lista);
 	    return modelAndView;
 	  }
-	
+	 //Método de delelar 
+    @DeleteMapping(value = "delete") /*mapeamento da url*/
+    @ResponseBody/*Descrição da resposta*/
+    public ResponseEntity<String> delete (@RequestParam Long id){
+    	
+    	prod.deleteById(id);
+    	
+    	return new ResponseEntity<String>("Usuario deletado com sucesso: ", HttpStatus.OK); 
+    }
 }
--- Contatos/src/main/java/com/contatos/Contatos/repository/Contatos.java
+++ Contatos/src/main/java/com/contatos/Contatos/repository/Contatos.java
@@ -1,6 +1,9 @@
 package com.contatos.Contatos.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.contatos.Contatos.model.Contato;
@@ -8,4 +11,6 @@ import com.contatos.Contatos.model.Contato;
 @Repository
 public interface Contatos extends JpaRepository<Contato, Long>{
 
+	@Query(value = "select * from contato where ativo = 1", nativeQuery = true)
+	List<Contato> findAllAtivas(); 
 }
