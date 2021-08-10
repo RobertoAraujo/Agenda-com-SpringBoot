ec9b108dac68566dd72cc93aa39a414d39320a7d
Roberto <robertomoraes@lampp-it.com.br>
2021-08-10 13:13:23 -0500

    Teste

--- Contatos/src/main/java/com/contatos/Contatos/controller/CadastroController.java
+++ Contatos/src/main/java/com/contatos/Contatos/controller/CadastroController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.contatos.Contatos.model.Contato;
 import com.contatos.Contatos.model.Produto;
+import com.contatos.Contatos.repository.Contatos;
 import com.contatos.Contatos.repository.Produtos;
 
 @Controller
 public class CadastroController {
 	@Autowired
 	private Produtos produt;
+	@Autowired
+	private Contatos contatos;
 	
-	// cadastra e salva no banco
+	// cadastra e salva no banco produto
 	@RequestMapping(path = "/cadastro",  method = RequestMethod.GET)
 	public ModelAndView cadastroGet(Produto produto){			
 		return new ModelAndView("cadastro");
@@ -26,6 +30,20 @@ public class CadastroController {
 		
 		return new ModelAndView("cadastro");
 	}
+	
+	// cadastra e salva no banco contatos
+		@RequestMapping(path = "/contatos-cadastro",  method = RequestMethod.GET)
+		public ModelAndView contatoCadastroGet(Contato contato){			
+			return new ModelAndView("contatos-cadastro");
+		}
+		
+		@RequestMapping(path = "/contatos-cadastro",  method = RequestMethod.POST)
+		public ModelAndView ContatoCadastroPost(Contato contato){			
+			contatos.save(contato);
+			
+			return new ModelAndView("contatos-cadastro");
+		}
+
 	
 	
 }
